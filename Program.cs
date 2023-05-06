9d22f46bf13dc50c35827726d9ace16b4354a3fa
unknown <stefanljusic10@gmail.com>
2023-05-06 20:14:41 +0200

    first commit dotnet

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/* Primer sadrzi izvorni kod za potrebe vezbanja sistema za kontrolu verzija.
+ * Sam izvorni kod u pocetnom stanju ne sadrzi nikakvu korisnu demonstraciju. */
+
+using System;
+
+namespace Termin10Primer04
+{
+    class Program
+    {
+        static void Metoda01()
+        {
+            Console.WriteLine("Da je cutanje snaga a govorenje slabost, vidi se i po tome sto starci i deca vole da pricaju.");
+        }
+
+        static void Metoda02()
+        {
+            // Prazna metoda
+        }
+
+        static void Metoda03()
+        {
+            // Prazna metoda
+        }
+
+        static void Metoda04()
+        {
+            // Prazna metoda
+        }
+
+        static void Main(string[] args)
+        {
+            Metoda01();
+            Metoda02();
+            Metoda03();
+            Metoda04();
+
+            Console.ReadLine();
+        }
+    }
+}
